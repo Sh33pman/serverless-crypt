2de6157fbaba6083fcf8da3126ee8708ab65179d
Johannes Gryffenberg <sh33pman@Johanness-MacBook-Pro.local>
2017-03-06 16:56:56 +0200

    Added support to allow different AWS KMS resources for each stage. (WIP)

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const decrypt = require('./lib/decrypt');
 const saveSecret = require('./lib/saveSecret');
 const addLibraries = require('./lib/addLibraries');
 const removeLibraries = require('./lib/removeLibraries');
-
-const SECRET_FILE = '.serverless-secret.json';
+const addSecretFile = require('./lib/addSecretFile');
+const removeSecretFile = require('./lib/removeSecretFile');
 
 class Crypt {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options || {};
     this.provider = this.serverless.getProvider('aws');
-    this.secret_file = SECRET_FILE;
-
+    this.secret_file  = '.serverless-secret-' + (this.options.stage || this.serverless.service.provider.stage) + '.json';
+    
     Object.assign(
       this,
       validate,
@@ -24,7 +24,9 @@ class Crypt {
       decrypt,
       saveSecret,
       addLibraries,
-      removeLibraries
+      removeLibraries,
+      addSecretFile,
+      removeSecretFile
     );
 
     this.commands = {
@@ -39,12 +41,17 @@ class Crypt {
             shortcut: 'n',
             required: true,
           },
+          stage: {
+            usage: 'Name of the stage',
+            shortcut: 's',
+            required: true,
+          },
           text: {
             usage: 'Plaintext to encrypt',
             shortcut: 't',
           },
           save: {
-            usage: `Save the encrypted secret (to ${SECRET_FILE}`,
+            usage: `Save the encrypted secret (to ${this.secret_file}`,
           },
         },
       },
@@ -66,16 +73,20 @@ class Crypt {
     this.hooks = {
       'before:deploy:function:deploy': () => BbPromise.bind(this)
         .then(this.validate)
-        .then(this.addLibraries),
+        .then(this.addLibraries)
+        .then(this.addSecretFile),
       'after:deploy:function:deploy': () => BbPromise.bind(this)
         .then(this.validate)
-        .then(this.removeLibraries),
+        .then(this.removeLibraries)
+        .then(this.removeSecretFile),
       'before:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
         .then(this.validate)
-        .then(this.addLibraries),
+        .then(this.addLibraries)
+        .then(this.addSecretFile),
       'after:deploy:deploy': () => BbPromise.bind(this)
         .then(this.validate)
-        .then(this.removeLibraries),
+        .then(this.removeLibraries)
+        .then(this.removeSecretFile),
       'encrypt:encrypt': () => BbPromise.bind(this)
         .then(this.validate)
         .then(this.encrypt)
